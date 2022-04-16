e7c7bdda5942d2104b40552518568baaf4792299
npee <jmc4664@gmail.com>
2022-04-16 16:53:01 +0900

    DriverManager 테스트

---
+++ src/test/java/io/npee/jdbc/connection/ConnectionTest.java
@@ -0,0 +1,23 @@
+package io.npee.jdbc.connection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static io.npee.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+public class ConnectionTest {
+
+    @Test
+    void driverManager() throws SQLException {
+        Connection conn1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Connection conn2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        log.info("conn1 {}, {}", conn1, conn1.getClass());
+        log.info("conn2 {}, {}", conn2, conn2.getClass());
+    }
+
+}
