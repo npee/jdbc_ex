6e992c5d60a27a9f637e77ee940b7d27b04de29e
npee <jmc4664@gmail.com>
2022-04-16 17:12:13 +0900

    DriverManagerDataSource 테스트

--- src/test/java/io/npee/jdbc/connection/ConnectionTest.java
+++ src/test/java/io/npee/jdbc/connection/ConnectionTest.java
@@ -2,7 +2,9 @@ package io.npee.jdbc.connection;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -16,6 +18,19 @@ public class ConnectionTest {
     void driverManager() throws SQLException {
         Connection conn1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         Connection conn2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        log.info("conn1 {}, {}", conn1, conn1.getClass());
+        log.info("conn2 {}, {}", conn2, conn2.getClass());
+    }
+
+    @Test
+    void dataSourceDriverManager() throws SQLException {
+        DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        useDataSource(dataSource);
+    }
+
+    private void useDataSource(DataSource dataSource) throws SQLException {
+        Connection conn1 = dataSource.getConnection();
+        Connection conn2 = dataSource.getConnection();
         log.info("conn1 {}, {}", conn1, conn1.getClass());
         log.info("conn2 {}, {}", conn2, conn2.getClass());
     }
