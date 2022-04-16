55c33fc1c13688f4ae24e988f0b137e81c4eb83b
npee <jmc4664@gmail.com>
2022-04-16 17:29:28 +0900

    HikariCP 테스트

--- src/test/java/io/npee/jdbc/connection/ConnectionTest.java
+++ src/test/java/io/npee/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package io.npee.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -26,6 +27,22 @@ public class ConnectionTest {
     void dataSourceDriverManager() throws SQLException {
         DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         useDataSource(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
+        // 별도의 쓰레드를 사용해서 Pool을 채운다.
+        // ...10 connections added
+        // After adding stats (total=10, active=2, idle=8, waiting=0)
     }
 
     private void useDataSource(DataSource dataSource) throws SQLException {
