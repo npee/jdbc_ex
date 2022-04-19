9ce839759ccd46dcf676cf5834aa051e1a514490
npee <jmc4664@gmail.com>
2022-04-19 21:53:46 +0900

    계좌이체 서비스 예외 테스트

--- src/test/java/io/npee/jdbc/service/MemberServiceV1Test.java
+++ src/test/java/io/npee/jdbc/service/MemberServiceV1Test.java
@@ -4,6 +4,7 @@ import io.npee.jdbc.connection.ConnectionConst;
 import io.npee.jdbc.domain.Member;
 import io.npee.jdbc.repsoitory.MemberRepositoryV1;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,6 +31,13 @@ class MemberServiceV1Test {
         memberService = new MemberServiceV1(memberRepository);
     }
 
+    @AfterEach
+    void after() throws SQLException {
+        memberRepository.delete(MEMBER_A);
+        memberRepository.delete(MEMBER_B);
+        memberRepository.delete(MEMBER_EX);
+    }
+
     @Test
     @DisplayName("정상 이체")
     void accountTransfer() throws SQLException {
@@ -44,5 +52,22 @@ class MemberServiceV1Test {
         Member findMemberB = memberRepository.findById(memberB.getMemberId());
         Assertions.assertThat(findMemberA.getMoney()).isEqualTo(8000);
         Assertions.assertThat(findMemberB.getMoney()).isEqualTo(12000);
+    }
+
+    @Test
+    @DisplayName("이체 중 예외 발생")
+    void accountTransferEx() throws SQLException {
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberEx = new Member(MEMBER_EX, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberEx);
+
+        Assertions.assertThatThrownBy(() -> memberService.accountTransfer(memberA.getMemberId(), memberEx.getMemberId(), 2000))
+                .isInstanceOf(IllegalStateException.class);
+
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberB = memberRepository.findById(memberEx.getMemberId());
+        Assertions.assertThat(findMemberA.getMoney()).isEqualTo(8000);
+        Assertions.assertThat(findMemberB.getMoney()).isEqualTo(10000);
     }
 }
